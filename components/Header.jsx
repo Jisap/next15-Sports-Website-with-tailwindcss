5a076192ec47fc6d97d0c1f71c794f906f6ee305
Jorge Matos <jisapjisap@gmail.com>
2025-09-04 19:47:09 +0200

    Enhance Header component and add new assets
    - Refactored the Header component to improve layout and include a logo with Sidenav for better navigation.
    - Updated global styles in `globals.css` to adjust container padding and added a new button style.
    - Introduced multiple new SVG icons and images for enhanced visual elements across the application.

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,20 +9,51 @@ import Sidenav from "./Sidenav"
 
 const Header = () => {
   return (
-    <div className="xl:absolute max-w-[1440px] mx-auto left-0 right-0 bg-pink-200">
+    <header className="xl:absolute max-w-[1440px] mx-auto left-0 right-0">
       <div className="container mx-auto">
         <div className="flex justify-between items-center py-10">
           {/* logo & sidenav */}
-          <div>logo & sidenav</div>
+          <div className="flex items-center justify-between w-full md:w-auto gap-8">
+            <Link href={"/"}>
+              <Image 
+                src="/assets/logo.svg"
+                width={226}
+                height={48}
+                alt="logo"
+              />
+            </Link>
+            <div className="xl:hidden">
+              <Sidenav />
+            </div>
+          </div>
 
           {/* nav.(desktop) */}
-          <nav>nav</nav>
+          <nav className="hidden xl:flex gap-6 pl-24 text-primary">
+            <a href="#" className="link-primary hover:text-accent2">Men</a>
+            <a href="#" className="link-primary hover:text-accent2">Women</a>
+            <a href="#" className="link-primary hover:text-accent2">Kids</a>
+            <a href="#" className="link-primary hover:text-accent2">Sale</a>
+          </nav>
 
           {/* search, favorites, cart */}
-          <div>search, favorites, cart</div>
+          <div className="hidden md:flex items-center justify-end gap-4 w-[360px]">
+            <input 
+              type="search" 
+              placeholder="Search ..." 
+              className="input w-[126px] focus:w-[200px] transition-all pl-7" 
+            />
+
+            <button className="btn-icon btn-outline group">
+              <FaHeart className="text-primary group-hover:text-primary/90" />
+            </button>
+            
+            <button className="btn-icon btn-accent">
+              <FaCartShopping className="text-primary" />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </header>
   )
 }
 
