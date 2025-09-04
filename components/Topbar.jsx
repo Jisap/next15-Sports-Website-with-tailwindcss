4e6197ec975f9924cdacc06223aaa11a121a82d5
Jorge Matos <jisapjisap@gmail.com>
2025-09-04 16:19:36 +0200

    Add react-icons dependency and update Topbar component
    - Added `react-icons` package to `package.json` and `package-lock.json`.
    - Enhanced the `Topbar` component to include a truck icon and a shipping message for improved user experience.

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -1,8 +1,15 @@
-
+import { FaTruck } from "react-icons/fa"
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <div className="bg-primary h-10 text-center text-white">
+      <div className="container mx-auto h-full flex items-center justify-center">
+        <div className="flex items-center gap-2">
+          <FaTruck className="" />
+          <p className="text-sm">Complementary global shipping</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
