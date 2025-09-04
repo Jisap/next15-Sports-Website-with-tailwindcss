27c6a2df27fcf5f3a3572853fbb53e4604f5db9d
Jorge Matos <jisapjisap@gmail.com>
2025-09-04 21:05:22 +0200

    Implement Latest section and add Slider component
    - Updated the Latest component to include a section with a title and a link to view all items.
    - Introduced a new Slider component for future use in displaying content.

--- components/Latest.jsx
+++ components/Latest.jsx
@@ -1,8 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Latest = () => {
   return (
-    <div>Latest</div>
+    <section className='mt-24'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-end mb-[72px]'>
+          <h4 className="h4 w-max border-b-2">
+            Explore Our Lates Collection
+          </h4>
+          <Link href="/" className="linl-primary text-accent2 hover:text-primary">
+            View All
+          </Link>
+        </div>
+        {/* Slider */}
+      </div>
+    </section>
   )
 }
 
