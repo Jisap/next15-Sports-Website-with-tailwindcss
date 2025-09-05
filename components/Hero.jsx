41eaf58b928b057cf525e30b60cf2e8a81b9f0c4
Jorge Matos <jisapjisap@gmail.com>
2025-09-05 13:31:59 +0200

    Refactor Hero component with new layout and background image
    - Updated the Hero component to include a new background image and enhanced layout for better visual appeal.
    - Added structured sections for text and images, improving content organization and user engagement.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,32 @@
+import { FaArrowRight, FaFire, FaPlay } from "react-icons/fa"
+
 
 
 const Hero = () => {
   return (
-    <div className="bg-[#f6f6f6] xl:h-[1064px]">
-      Hero
+    <div className="bg-[#f6f6f6] xl:h-[1064px] bg-[url(/assets/img/hero/bg.png)] bg-no-repeat bg-[60%] md:bg-right-bottom xl:bg-right">
+      <div className="container mx-auto flex items-center h-full">
+        <div className="relative flex flex-col xl:flex-row items-center w-full">
+          
+          {/* text */}
+          <div>
+            {/* badge & text */}
+            <div>
+              badge & text
+            </div>
+            {/* h1 */}
+            <h1 className="h1 mb-[40px]">h1</h1>
+            {/* btns */}
+            <div>btns</div>
+          </div>
+
+          {/* img */}
+          <div className="relative xl:absolute xl:-right-[100px] xl:-top-7">
+            <img src="assets/img/hero/running.png" alt="" />
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
