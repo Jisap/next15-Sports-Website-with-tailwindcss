5b359952a0f141220ad7ebbaff436fa61b78ec68
Jorge Matos <jisapjisap@gmail.com>
2025-09-06 19:29:33 +0200

    Add primary button styles and update Cta component layout
    - Introduced new styles for the primary button in globals.css to enhance visual appeal and interactivity.
    - Refactored the Cta component to include a structured layout with a heading and a primary button, improving user engagement.
    - Commented out an unused div in the page component for cleaner code.

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { FaArrowRight } from "react-icons/fa"
+
+
+
 
 const Cta = () => {
   return (
-    <div>Cta</div>
+    <section className='mt-24 bg-accent py-20'>
+      <div className='container mx-auto'>
+        <div className="flex flex-col xl:flex-row items-center justify-between">
+          <h3 className="h3 text-center mb-6 xl:mb-0">Founded with passion for excellence</h3>
+          <button className="btn btn-primary gap-2.5">
+            Browse our collection
+          </button>
+        </div> 
+      </div>
+    </section>
   )
 }
 
