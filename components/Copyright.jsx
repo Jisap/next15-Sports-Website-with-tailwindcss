2653809b573c4f50f71addd542d3ae6c7518504b
Jorge Matos <jisapjisap@gmail.com>
2025-09-07 12:23:52 +0200

    Revamp Copyright component with structured layout and navigation links
    - Redesigned the Copyright component to include a section layout with navigation links for Terms of Use, Cooking Settings, and Privacy Policy.
    - Enhanced visual presentation with a new background and improved styling for better user engagement.
    - Updated copyright notice to reflect the current year and brand name.

--- components/Copyright.jsx
+++ components/Copyright.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Copyright = () => {
   return (
-    <div>Copyright</div>
+    <section className='bg-primary mt-24 py-12 text-white'>
+      <div className='container mx-auto'>
+        <ul className='flex flex-col xl:flex-row items-center justify-center gap-6'>
+          <li>
+            <a href="#" className='border-b'>Terms of use</a>
+          </li>
+          <li>
+            <a href="#" className='border-b'>Cookings Setting</a>
+          </li>
+          <li>
+            <a href="#" className='border-b'>Privacy Policy</a>
+          </li>
+        </ul>
+
+        <p>
+          &copy; 2025 Venoma. All rights reserved.
+        </p>
+      </div>
+    </section>
   )
 }
 
